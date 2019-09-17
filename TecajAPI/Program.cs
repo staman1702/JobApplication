aadeea5e95a17d0b2d9b43972e95f5f8bfef70e6
staman1702 <staman1702@gmail.com>
2019-09-17 14:09:43 +0200

    added ProsjecanTecajHrkUsdAsync

---
+++ TecajAPI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TecajAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProsjecanTecajHrkUsdAsync().Wait();
+        }
+
+        static async Task ProsjecanTecajHrkUsdAsync()
+        {
+            double sum = 0;
+            double avrgRate;
+
+            for (int i = 0; i >= -4; i--)
+            {
+                using (var client = new HttpClient())
+                {
+
+                    string date = DateTime.Now.AddDays(i).ToString("yyyy-MM-dd");
+                    string ext = date + "?base=HRK";
+
+                    client.BaseAddress = new Uri("https://api.exchangeratesapi.io/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept
+                        .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await client.GetAsync(ext);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        RootObject root = await response.Content.ReadAsAsync<RootObject>();
+                        sum += root.rates.USD;
+                    }
+
+                }
+            }
+
+            avrgRate = sum / 5;
+
+            Console.WriteLine("According to https://api.exchangeratesapi.io," +
+                " \naverage HRK rate in past 5 days is {0} USD.", avrgRate);
+        }
+    }
+}
---
+++ TecajAPI/RootObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TecajAPI
+{
+    public class RootObject
+    {
+        public Rates rates { get; set; }
+        public string @base { get; set; }
+        public string date { get; set; }
+    }
+}
